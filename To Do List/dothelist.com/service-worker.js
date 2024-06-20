d077124a6c4a859a7611a4752295ab29555a1235
PixxelCraft22 <168555574+PixxelCraft22@users.noreply.github.com>
2024-06-20 13:16:54 +0200

    Add files via upload

---
+++ To Do List/dothelist.com/service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'todo-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/service-worker.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (!cacheWhitelist.includes(key)) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+});
